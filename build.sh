da13f7d2c4cdde52bb98b84c41efbb16f53c26ca
Johnnie Birch <johnnie.l.birch.jr@intel.com>
2023-08-28 21:26:01 -0700

    Update simplify config.inc

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 IMAGE_NAME="wasmscore"
 ARCH=$(uname -m | awk '{print tolower($0)}')
 KERNEL=$(uname -s | awk '{print tolower($0)}')
-echo "Building ${IMAGE_NAME} for $ARCH based on wasmtime@${WASMTIME_BUILD}"
+echo "Building ${IMAGE_NAME} version ${IMAGE_VER} for $ARCH."
 
 # Create Docker Image
 docker build -t ${IMAGE_NAME} --build-arg ARCH=$(uname -m) .
